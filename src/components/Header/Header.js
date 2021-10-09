161560c4f19d20f842a4fa7d5f6bd87cf72f8f43
Sanjay Prajapati <sanjay270899@gmail.com>
2021-10-10 05:10:22 +0530

    ✨ Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-green-500 text-center text-2xl text-white font-bold p-3 shadow-lg">
+      Blockchain Demo
+    </header>
+  );
+};
+
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
+export { default as Header } from "./Header";
+export * from "./Header";
+
 export { default as Input } from "./Input";
 export * from "./Input";
